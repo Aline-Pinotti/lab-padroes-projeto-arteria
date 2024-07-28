f7d70f2e1b2ca4db98bfe1db07f383facb29e747
user.name: Aline-Pinotti <user.email: alinefpinotti@gmail.com>
2024-07-28 20:32:17 -0300

    Projeto criado

---
+++ arteria/src/main/java/com/arteria/arteria/ArteriaApplication.java
@@ -0,0 +1,13 @@
+package com.arteria.arteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArteriaApplication.class, args);
+	}
+
+}
---
+++ arteria/src/test/java/com/arteria/arteria/ArteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arteria.arteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
