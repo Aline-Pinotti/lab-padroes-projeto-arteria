5fad733f5f52474ca02c4b16d09a962ba18e12b4
user.name: Aline-Pinotti <user.email: alinefpinotti@gmail.com>
2024-07-28 21:52:54 -0300

    add final

--- arteria/src/main/java/com/arteria/arteria/ArteriaApplication.java
+++ arteria/src/main/java/com/arteria/arteria/ArteriaApplication.java
@@ -2,6 +2,7 @@ package com.arteria.arteria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * Projeto Spring Boot gerado via Spring Initializr.
